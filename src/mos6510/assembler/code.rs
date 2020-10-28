f7d354f0e865d2abdbeb226da8f6d5da7dfe7dcc
Robert Jaremczak <rjaremczak@gmail.com>
2020-10-28 21:58:10 +0100

    refactoring, relocating modules

---
+++ src/mos6510/assembler/code.rs
@@ -0,0 +1,53 @@
+use super::error::AsmError;
+
+pub struct ObjectCode {
+    pub write_enabled: bool,
+    pub origin: u16,
+    pub location_counter: u16,
+    pub data: Vec<u8>,
+}
+
+impl ObjectCode {
+    pub fn new(origin: u16) -> Self {
+        Self {
+            origin,
+            location_counter: origin,
+            data: Vec::new(),
+            write_enabled: false,
+        }
+    }
+
+    pub fn emit_byte(&mut self, byte: u8) {
+        self.location_counter += 1;
+        if self.write_enabled {
+            self.data.push(byte);
+        }
+    }
+
+    pub fn emit_word(&mut self, word: u16) {
+        self.emit_byte(word as u8);
+        self.emit_byte((word >> 8) as u8);
+    }
+
+    pub fn set_location_counter(&mut self, addr: u16) -> AsmError {
+        if addr >= self.location_counter {
+            let lc = self.location_counter;
+            self.location_counter = lc.saturating_add(addr);
+            if self.write_enabled {
+                for _ in lc..self.location_counter {
+                    self.data.push(0)
+                }
+            }
+            AsmError::ValueOutOfRange
+        } else {
+            AsmError::Ok
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    //fn
+}
---
+++ src/mos6510/assembler/error.rs
@@ -0,0 +1,14 @@
+#[derive(Debug)]
+pub enum AsmError {
+    Ok,
+    SymbolNotDefined,
+    MissingOperand,
+    NumericOperandRequired,
+    SyntaxError,
+    CommandProcessingError,
+    ValueOutOfRange,
+    InvalidMnemonic,
+    InvalidInstructionFormat,
+    InvalidPhase,
+    MalformedOperand(std::num::ParseIntError),
+}
