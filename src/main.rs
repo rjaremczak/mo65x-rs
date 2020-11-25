e6e3e3f7ae06b65927c900a40f13c6976f7fe47b
Robert Jaremczak <rjaremczak@gmail.com>
2020-11-25 17:14:07 +0100

    crossterm added, press any key working

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,12 @@ use std::path::PathBuf;
 use std::time::Duration;
 use structopt::StructOpt;
 
+use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyModifiers};
+use crossterm::style::Print;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
+use crossterm::{cursor, execute};
+use std::io::{stdout, Write};
+
 #[derive(Debug, StructOpt)]
 #[structopt(author, about = "My Own 65xx emulator and more...")]
 struct CliOpt {
@@ -55,7 +61,7 @@ enum Mode {
 fn main() {
     let cliopt = CliOpt::from_args();
     println!("cliopt: {:#?}", cliopt);
-    let result = match cliopt.mode {
+    let _ = match cliopt.mode {
         Mode::Asm { src, bin } => assemble(src, bin),
         Mode::Dis { addr: origin, bin } => disassemble(origin, bin),
         Mode::Run {
@@ -65,6 +71,17 @@ fn main() {
         } => run(origin, bin, freq_khz),
         Mode::Con => console(),
     };
+}
+
+fn wait_for_any_key() {
+    enable_raw_mode().unwrap();
+    loop {
+        if let Event::Key(_) = read().unwrap() {
+            break;
+        }
+    }
+
+    disable_raw_mode().unwrap();
 }
 
 fn assemble(src: PathBuf, bin: Option<PathBuf>) -> Result<(), AppError> {
@@ -89,9 +106,8 @@ fn run(origin: u16, bin: PathBuf, freq_khz: u32) -> Result<(), AppError> {
     let mut emulator = Emulator::new();
     emulator.init();
     emulator.run();
-    println!("Emulator is running, press ENTER to quit");
-    std::thread::sleep(Duration::from_secs(1));
-    let _ = io::stdin().read_line(&mut String::new());
+    println!("Emulator is running, press any key to quit");
+    wait_for_any_key();
     emulator.stop();
     println!("stop");
     Ok(())
